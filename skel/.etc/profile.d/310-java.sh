30c917234cb098c5914041879be470efc0c1e082
Giovanni Pelosi <hute37@gmail.com>
2018-03-21 17:29:38 +0100

    java 7/8 + spark env

--- skel/.etc/profile.d/310-java.sh
+++ skel/.etc/profile.d/310-java.sh
@@ -8,7 +8,19 @@
 
 
 SYSJAVACONF="/opt/java/etc/java.conf"
-USRJAVACONF="$HOME/.etc/java.conf"
+
+if [ -f ~/.java-8.on ]; then 
+	USRJAVACONF="$HOME/.etc/java8.conf"
+else if [ -f ~/.java-7.on ]; then
+	USRJAVACONF="$HOME/.etc/java7.conf"
+else if [ -f ~/.java.on ]; then
+	USRJAVACONF="$HOME/.etc/java.conf"
+else
+	USRJAVACONF=""
+fi
+fi
+fi
+
 
 JAVA_CONF_SET=1
 
--- skel/.etc/profile.d/410-hadoop.sh
+++ skel/.etc/profile.d/410-hadoop.sh
@@ -11,28 +11,24 @@ SPARK_DEFAULT=/opt/srv/apache/spark
 
 if [ -d "$SPARK_DEFAULT" ]; then
 
-    SPARK_HOME="$SPARK_DEFAULT"
 
-    SPARK_MAJOR_VERSION=2
+        case "$HADOOP_VERSION"   in
+                2.6)
+                        SPARK_HOME=/opt/srv/apache/spark1
+                        SPARK_MAJOR_VERSION=1
+                        ;;
+                2.7)
+                        SPARK_HOME=/opt/srv/apache/spark2
+                        SPARK_MAJOR_VERSION=2
+                        ;;
+                *)
+                        SPARK_HOME=/opt/srv/apache/spark2
+                        SPARK_MAJOR_VERSION=2
+                        ;;
+        esac
+        export SPARK_HOME
+        export SPARK_MAJOR_VERSION
     
-    # PATH=$PATH:$SPARK_HOME/bin
-    # PATH=$SPARK_HOME/bin:$PATH
-    # [ -z "$JAVA_HOME" ] || PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
-    # export PATH
-
-
-    # PYSPARK_PYTHON=$CONDA_HOME/bin/python3
-    # PYTHONHASHSEED=0
-    
-    # LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_HOME/lib
-    # LD_LIBRARY_PATH=$CONDA_HOME/lib:$LD_LIBRARY_PATH
-    # export LD_LIBRARY_PATH
-
-    export SPARK_HOME
-    export SPARK_MAJOR_VERSION
-    
-    # export PYSPARK_PYTHON
-    # export PYTHONHASHSEED
     
 fi
 
