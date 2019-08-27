0afed98dac1b4720fea647c0cfe1f632ec7b5640
hute37 <hute37@gmail.com>
2019-08-27 16:12:12 +0200

    bibtex support

---
+++ docs/org/ref/auto/ref-sample.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook
+ "ref-sample"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "albonico-2018-limit-asset"
+    "friebel-2018-inter-migrat"
+    "albonico-2017-debun-myth"))
+ :bibtex)
+
