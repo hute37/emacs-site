5f53d268e8c54c6b9149bbe25c1d2ff19cfe9d9e
hute37 <hute37@gmail.com>
2021-06-13 16:06:18 +0200

    etc profile pyenv+poetry

--- skel/.etc/profile.d/350-python.bash
+++ skel/.etc/profile.d/350-python.bash
@@ -1,27 +1,18 @@
 # -*- mode: shell-script;-*-
-
 ##
-#  virtualenv (wrapper) python environment
+#  pyenv environment
 #
+if [ "$PY_RC_ENV" ]; then
 
-venv_wrap_bash() {
+py_rc_env() {
 
-export SYS_PYTHON=`which python`
-export VIRTUALENVWRAPPER_PYTHON="$SYS_PYTHON"
-export VIRTUALENVWRAPPER_SCRIPT=`which virtualenvwrapper.sh`
-
-export VENV_CONFIG=/etc/venv.conf
-export WORKON_HOME=/usr/local/lib/pythonenvs
-
-set -a
-[ -r "$VENV_CONFIG" ] && source $VENV_CONFIG || true
-set +a
-
-[ -n "$VIRTUALENVWRAPPER_SCRIPT" ] && \
-[ -x "$VIRTUALENVWRAPPER_SCRIPT" ] && source $VIRTUALENVWRAPPER_SCRIPT || true
-
-if [ -n "$VENV_NAME" ] ; then 
-    workon "$VENV_NAME"
-fi
+eval "$(pyenv init -)"
 
 }
+
+# in ~/.zshrc
+# py_rc_env
+
+
+fi
+
--- skel/.etc/profile.d/350-python.sh
+++ skel/.etc/profile.d/350-python.sh
@@ -1,11 +1,61 @@
 #!/bin/sh
 # -*- mode: shell-script;-*-
+export PY_RC_PROFILE=1
+
+
+##
+#  pyenv environment
+#
+if [ ! -f ~/.py-env.off ]; then
+if [ -d $HOME/.pyenv ]; then    
+
+    export PY_RC_ENV=1
+
+py_rc_env_sh() {
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
+
+}
+
+py_rc_env_sh
+
+fi
+fi
+
+
+##
+#  poetry environment
+#
+if [ ! -f ~/.py-poetry.off ]; then
+if [ -d $HOME/.poetry ]; then    
+
+    export PY_RC_POETRY=1
+
+py_rc_poetry_sh() {
+
+export PATH="$HOME/.poetry/bin:$PATH"
+
+}
+
+py_rc_poetry_sh
+
+fi
+fi
+
+
+
 
 ##
 #  virtualenv (wrapper) python environment
 #
+if [ -f ~/.py-venv.on ]; then
+    export PY_RC_VENV=1
+    
 
-venv_wrap_sh() {
+py_rc_venv_wrap_sh() {
 
 SYS_PYTHON=`which python`
 VIRTUALENVWRAPPER_PYTHON="$SYS_PYTHON"
@@ -26,3 +76,9 @@ set -a
 set +a
 
 }
+
+py_rc_venv_wrap_sh
+
+fi
+
+
