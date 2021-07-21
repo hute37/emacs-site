3b285ed0282f0d5a83f2ed2964013884f71aaa55
hute37 <hute37@gmail.com>
2021-07-21 15:01:04 +0200

    tiliz session sort

--- skel/.config/tilix/tilix-rc.sh
+++ skel/.config/tilix/tilix-rc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-tilix6() { export TILIX_SESSION=6; tilix $(find ~/.config/tilix/sx/$TILIX_SESSION -name '*.json' | sed -e 's/^/-s /' | tr '\n' ' '); }
+tilix6() { export TILIX_SESSION=6; tilix $(find ~/.config/tilix/sx/$TILIX_SESSION -name '*.json' | sort | sed -e 's/^/-s /' | tr '\n' ' '); }
 
 
