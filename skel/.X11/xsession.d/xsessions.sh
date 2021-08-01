6cf49aab373251968cb2a6cd34dd058aefc86efd
Giovanni Pelosi <hute37@gmail.com>
2021-08-01 17:55:27 +0200

    .X11 refactor, mod3: Hyper

---
+++ skel/.X11/xsession.d/xsessions.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ -z "XSESSIONS_ENTER" ]; then
+    export XSESSIONS_ENTER=1
+    exec $SHELL $0 $@
+fi
+unset XSESSIONS_ENTER
+
+: ${XSESSIONS_NAME:=${1:-$DESKTOP_SESSION}}
+: ${XSESSIONS_FILE:=$HOME/.Xsessions.d/$XSESSIONS_NAME.sh}
+
+export XSESSIONS_NAME
+export XSESSIONS_FILE
+
+if [ ! -f $XSESSIONS_FILE ]; then
+    echo "xsessions - $(date -Isec) - ERROR: NOTFND - sx: $XSESSIONS_NAME, file: $XSESSIONS_FILE"
+    exit 1
+fi
+
+echo "xsessions - $(date -Isec) - info: start - sx: $XSESSIONS_NAME, file: $XSESSIONS_FILE, ..."
+
+set -x
+. $XSESSIONS_FILE
+set +x
+
+export rc=$?
+
+echo "xsessions - $(date -Isec) - info: start - sx: $XSESSIONS_NAME, file: $XSESSIONS_FILE, (rc=$rc) done."
+
+exit $rc
