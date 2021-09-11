ed40bd737b804f69ee762a3ec334c871c1c4f122
Giovanni Pelosi <hute37@gmail.com>
2021-09-11 22:26:17 +0200

    openbox,alacritty 4 termite

--- skel/.local/bin/_xrandr-def.sh
+++ skel/.local/bin/_xrandr-def.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-xrandr --output Virtual-1 --mode 1360x768
 
+case "$(hostname)" in
+    heka)
+        xrandr --output VGA-1-1 --primary --output LVDS-1-1 --right-of VGA-1-1
+        ;;
+    eos)
+        xrandr --output Virtual-1 --mode 1360x768
+        ;;
+    *)
+        xrandr --output Virtual-1 --mode 1360x768
+        ;;
+esac
