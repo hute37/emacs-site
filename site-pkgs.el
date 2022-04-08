4be75ef7ea646de62cfea89b776157601fb3b66c
Giovanni Pelosi <hute37@gmail.com>
2022-04-08 16:10:40 +0200

    kitty conf, helm reload

--- site-pkgs.el
+++ site-pkgs.el
@@ -2812,18 +2812,19 @@ the automatic filling of the current paragraph."
   :diminish helm-mode
   :init
   (progn
-    (require 'helm-config)
-    (message "#helm(1): '( (h7/use-helm . %s) )" (h7/use-helm)) 
-    (setq helm-candidate-number-limit 100)
-    ;; From https://gist.github.com/antifuchs/9238468
-    (setq helm-idle-delay 0.0 ; update fast sources immediately (doesn't).
-          helm-input-idle-delay 0.01  ; this actually updates things
-                                        ; reeeelatively quickly.
-          helm-yas-display-key-on-candidate t
-          helm-quick-update t
-          helm-M-x-requires-pattern nil
-          helm-ff-skip-boring-files t)
-    (helm-mode))
+  ;;   (require 'helm-config)
+  ;;   (message "#helm(1): '( (h7/use-helm . %s) )" (h7/use-helm)) 
+  ;;   (setq helm-candidate-number-limit 100)
+  ;;   ;; From https://gist.github.com/antifuchs/9238468
+  ;;   (setq helm-idle-delay 0.0 ; update fast sources immediately (doesn't).
+  ;;         helm-input-idle-delay 0.01  ; this actually updates things
+  ;;                                       ; reeeelatively quickly.
+  ;;         helm-yas-display-key-on-candidate t
+  ;;         helm-quick-update t
+  ;;         helm-M-x-requires-pattern nil
+  ;; 
+    (helm-mode)
+    )
   :bind (("C-c h" . helm-mini)
          ("C-h a" . helm-apropos)
          ("C-x C-b" . helm-buffers-list)
