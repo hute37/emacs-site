39a0edb165adb86d7eb3b1ec89d5a039abb614bb
hute37 <hute37@gmail.com>
2022-06-20 15:44:01 +0200

    cache/docs (oneliners), xah-space-to-newline

--- site-func.el
+++ site-func.el
@@ -64,6 +64,63 @@
     (message "Setting truncate-lines to nil"))
   )
 
+
+(defun xah-space-to-newline ()
+  "Replace space sequence to a newline char.
+Works on current block or selection.
+
+URL `http://xahlee.info/emacs/emacs/emacs_space_to_newline.html'
+Version 2017-08-19"
+  (interactive)
+  (let* ( $p1 $p2 )
+    (if (use-region-p)
+        (progn
+          (setq $p1 (region-beginning))
+          (setq $p2 (region-end)))
+      (save-excursion
+        (if (re-search-backward "\n[ \t]*\n" nil "move")
+            (progn (re-search-forward "\n[ \t]*\n")
+                   (setq $p1 (point)))
+          (setq $p1 (point)))
+        (re-search-forward "\n[ \t]*\n" nil "move")
+        (skip-chars-backward " \t\n" )
+        (setq $p2 (point))))
+    (save-excursion
+      (save-restriction
+        (narrow-to-region $p1 $p2)
+        (goto-char (point-min))
+        (while (re-search-forward " +" nil t)
+          (replace-match "\n" ))))))
+
+(defun xah-newline-to-space () ;; xah-space-to-newline-2022-06-08
+  "Replace space sequence to a newline char in current block or selection.
+
+If `universal-argument' is called first, ask user to enter a character to replace.
+
+URL `http://xahlee.info/emacs/emacs/emacs_space_to_newline.html'
+Version: 2017-08-19 2021-11-28"
+  (interactive)
+  (let* ( $p1 $p2 )
+    (if (use-region-p)
+        (progn
+          (setq $p1 (region-beginning))
+          (setq $p2 (region-end)))
+      (save-excursion
+        (if (re-search-backward "\n[ \t]*\n" nil "move")
+            (progn (re-search-forward "\n[ \t]*\n")
+                   (setq $p1 (point)))
+          (setq $p1 (point)))
+        (re-search-forward "\n[ \t]*\n" nil "move")
+        (skip-chars-backward " \t\n" )
+        (setq $p2 (point))))
+    (save-excursion
+      (save-restriction
+        (narrow-to-region $p1 $p2)
+        (goto-char (point-min))
+        (while (re-search-forward "\n+" nil t)
+          (replace-match " " ))))))
+
+
 ;; ---( kbd-macro )-----------------------------------------------------
 
 (defun start-or-end-kbd-macro ()
