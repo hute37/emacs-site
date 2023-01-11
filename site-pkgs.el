1e7440e47ec5e8f09e15e112a1841bcf62df0714
hute37 <hute37@gmail.com>
2023-01-11 02:48:52 +0100

    WIP: eshell arrow map

--- site-pkgs.el
+++ site-pkgs.el
@@ -3296,9 +3296,6 @@ the automatic filling of the current paragraph."
     (let ((inhibit-read-only t))
       (erase-buffer)
       (eshell-send-input)))
-  (add-hook 'eshell-mode-hook
-            '(lambda()
-               (local-set-key (kbd "C-l") 'eshell-clear-buffer)))
   (defun eshell-initialize ()
     (message "eshell:initialize >")
     (defun eshell-spawn-external-command (beg end)
@@ -3338,18 +3335,66 @@ the automatic filling of the current paragraph."
          (unintern 'eshell/sudo nil)))
 
     (message "eshell:initialize <"))
-  (defun eshell/e (file)
-      (find-file file))
-  (defun eshell/e (file)
-      (find-file file))
-  (defun eshell/ee (file)
-    (find-file-other-window file))
-  (defun eshell/emacs (file)
-    (find-file-other-window file))
+
+  (message "eshell:builtins >")
+
+  ;; @see: https://github.com/howardabrams/hamacs/blob/main/ha-eshell.org
+
+  (defun eshell/do (&rest args)
+    "Execute commands over lst. do chmod -x :: *.csv(x) "
+    (seq-let (cmd lst) (-split-on "::" args)
+      (dolist (file
+               (flatten-list (append lst)))
+        (add-to-list 'cmd file)
+        (eshell-named-command
+         (car cmd) (cdr cmd)))))
+
+  (defun eshell-fn-on-files (fun1 fun2 args)
+    "Call FUN1 on the first element in list, ARGS.
+     Call FUN2 on all the rest of the elements in ARGS."
+    (unless (null args)
+      (let ((filenames (flatten-list args)))
+        (funcall fun1 (car filenames))
+        (when (cdr filenames)
+          (mapcar fun2 (cdr filenames))))
+      ;; Return an empty string, as the return value from `fun1'
+      ;; probably isn't helpful to display in the `eshell' window.
+      ""))
+
+  (defun eshell/less (&rest files)
+    "Essentially an alias to the `view-file' function."
+    (eshell-fn-on-files 'view-file 'view-file-other-window files))
+
+  (defalias 'eshell/more 'eshell/less)
+  (defalias 'eshell/s 'eshell/less)
+
+  (defun eshell/e (&rest files)
+    "Essentially an alias to the `find-file' function."
+    (eshell-fn-on-files 'find-file 'find-file-other-window files))
+
+  (defun eshell/ee (&rest files)
+    "Edit one or more files in another window."
+    (eshell-fn-on-files 'find-file-other-window 'find-file-other-window files))
+
+  (defalias 'eshell/emacs 'eshell/e)
+  (defalias 'eshell/v 'eshell/e)
+
+  (message "eshell:builtins <")
   (message "eshell:preface <")
   :init
   (message "eshell:init >")
   (add-hook 'eshell-first-time-mode-hook 'eshell-initialize)
+  (add-hook 'eshell-mode-hook
+            (lambda()
+               (progn
+                 (local-set-key (kbd "C-l") 'eshell-clear-buffer)
+                 (unbind-key (kbd "<up>") eshell-mode-map)
+                 (unbind-key (kbd "<down>") eshell-mode-map)
+                 ;; (define-key eshell-mode-map (kbd "C-<up>") 'eshell-previous-matching-input-from-input)
+                 ;; (define-key eshell-mode-map (kbd "C-<down>") 'eshell-previous-matching-input-from-input)
+                 ;; (define-key eshell-mode-map (kbd "<up>") 'previous-line)
+                 ;; (define-key eshell-mode-map (kbd "<down>") 'next-line)
+                 )))
   ;; (use-package esh-toggle
   ;;   :ensure t
   ;;   :bind ("C-x C-h" . eshell-toggle))
@@ -3365,9 +3410,10 @@ the automatic filling of the current paragraph."
    eshell-history-size 5000
    eshell-buffer-maximum-lines 5000
    eshell-hist-ignoredups t
+   eshell-prefer-lisp-functions t
    eshell-scroll-to-bottom-on-input t
    eshell-destroy-buffer-when-process-dies t
-   eshell-visual-commands'("bash" "fish" "htop" "ssh" "top" "zsh"))
+   eshell-visual-commands'("bash" "fish" "vim" "nvim" "mc" "ranger" "htop" "ssh" "top" "zsh"))
 
 
   ;; ;; We want to use xterm-256color when running interactive commands
