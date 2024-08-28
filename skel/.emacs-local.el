09414b380d7a305d37158f1ffedd440c22c09f41
hute37 <hute37@gmail.com>
2024-08-28 11:29:57 +0200

    optional zmq

--- skel/.emacs-local.el
+++ skel/.emacs-local.el
@@ -2,6 +2,7 @@
  
  '(z-use-helm nil)
  '(z-use-pdf-tools t)
+ '(z-use-py-jupyter nil)
  '(z-var-global-bibliography '("~/work/bv/box-up/notes/ref/references.bib"))
  '(z-var-denote-directory "~/work/bv/box-up/notes/nat")
  
