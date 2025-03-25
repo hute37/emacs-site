4dbdbcbf0cee9985c216399bb4277e846dac44f6
hute37 <hute37@gmail.com>
2025-03-25 15:57:58 +0100

    xsession-nx

---
+++ skel/.X11/xinit.d/xsession-nx.sh
@@ -0,0 +1,14 @@
+# vim:set ft=sh sw=2 sts=2 et:
+
+#startxfce4
+#startlxde
+#startlxqt
+#dbus-launch openbox-session
+#dbus-launch awesome
+#dbus-launch i3
+#dbus-launch spectrwm
+/usr/bin/xterm -bg ghostwhite -fg black -cr green -bc -pc -ls -sl 10000 -sb -T Login -n login -geometry 230x30+0+50
+
+
+
+
